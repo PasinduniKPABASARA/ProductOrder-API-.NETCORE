acb4395aefb96c9aff6ce4edd6d8d3ecfbaa07da
PPK WICRAMAGE <127337691+PasinduniKPABASARA@users.noreply.github.com>
2024-09-02 13:25:32 +0530

    first commit

---
+++ Data/ProductOrderContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductOrder.Model;
+
+namespace ProductOrder.Data
+{
+    public class ProductOrderContext : DbContext
+    {
+        public ProductOrderContext (DbContextOptions<ProductOrderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductOrder.Model.Product> Product { get; set; } = default!;
+    }
+}
